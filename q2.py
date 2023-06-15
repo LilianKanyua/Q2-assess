9efb706bbba103c6ba2a07b39bb6821e885ca845
LilianKanyua <liliankanyua@gmail.com>
2023-06-15 09:21:35 +0300

    added changes

--- q2.py
+++ q2.py
@@ -1,26 +1,27 @@
+#   Question one
 class Story:
 
      def __init__(self,story_name,story_length,age_group,origin_language,african_community):
-         self.storyName = storyName
-         self.storyLength = storyLength
-         self.ageGroup = ageGroup
-         self.originlanguage = originlanguage
-         self.africanCommunity = africanCommunity
+         self.storyName = story_name
+         self.storyLength = story_length
+         self.ageGroup = age_group
+         self.originlanguage = origin_language
+         self.africanCommunity = african_community
      
 
     def display_details:
-        return f"This {self.storyName} is a story from the {self.africanCommunity} who speak the {self.originLanguage}.\n It is {self.storyLength} long and is meant for children who are {self.agegroup} years."
+        return f"This {self.story_name} is a story from the {self.african_community} who speak the {self.origin_language}.\n It is {self.story_length} long and is meant for children who are {self.age_group} years."
  
 
 
-class StoryTeller(Story):
+class Story_teller(Story):
 
     def __init__(self,story_name,story_length,age_group,origin_language,african_community):
     super .__init__():
 
 
-    def display_details(self,storyTellerName,numberOfMoralLessons):
-         return f"This {self.storyName} is narrated by {storyTellerName} in {self.originLanguage}, for children who are {self.ageGroup }. It is from the {self.africanCommunity community} and has {numberOfMoralLessons} moral lessons."
+    def display_details(self,story_teller_name,number_of_moralLessons):
+         return f"This {self.story_name} is narrated by {story_teller_name} in {self.origin_language}, for children who are {self.age_group }. It is from the {self.african_community} community and has {number_of_moral_lessons} moral lessons."
     
  
 
@@ -30,8 +31,16 @@ class Translator(Story):
     def __init__(self,story_name,story_length,age_group,origin_language,african_community):
     super .__init__():
 
-    def translateStory(self,origin_language,newLanguage):
-        return f"This {self.storyName} that was originally narrated in {self.originLanguage} from the {self.africanCommunity} community has now been translated to {newLanguage}."
+    def translateStory(self,origin_language,new_language):
+        return f"This {self.story_name} that was originally narrated in {self.origin_language} from the {self.african_community} community has now been translated to {new_language}."
 
 
- 
+#  Question two
+class African_Cuisine:
+
+     def __init__(self,unique_ingredients,prep_time,cooking_method,nutritional_value):
+         self.unique_ingredients = s
+         self.storyLength = storyLength
+         self.ageGroup = ageGroup
+         self.originlanguage = originlanguage
+         self.africanCommunity = africanCommunity
